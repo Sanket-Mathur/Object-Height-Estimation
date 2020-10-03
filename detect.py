d49bd3f6bd1a69c6e764b74bca85bd9daad20afb
Sanket-Mathur <rajeev.sanket@gmail.com>
2020-10-03 11:01:28 +0530

    Saving the values for next use and converted 2 files into 1

--- detect.py
+++ detect.py
@@ -1,23 +1,13 @@
 import cv2
 import numpy as np
+import pickle
 
-# importing user defined python file
-import parameters
 
+save = open('Values/save.dat', 'rb')
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 400)
 cap.set(4, 200)
-
-# Hard coded values
-h_min = 150
-h_max = 190
-s_min = 195
-s_max = 255
-v_min = 195
-v_max = 255
-threshold1 = 0
-threshold2 = 225
 
 
 # detecting the standard in the image and marking it
@@ -34,9 +24,50 @@ def getContour(img, imgContour):
 			cv2.rectangle(imgContour, (x,y), (x+w, y+h), (0,255,0), 5)
 
 
+def empty(a):
+	pass
+
+# spawns a Window for changing the parameters
+def takeVal():
+
+	img = cv2.imread('Parameters.png')[:160, :, :]
+	img = cv2.resize(img, (340, 120))
+	
+	cv2.namedWindow('Parameters')
+	cv2.imshow('Parameters', img)
+	cv2.resizeWindow('Parameters', 640, 240)
+	cv2.createTrackbar('HUE Min', 'Parameters', 150, 255, empty)
+	cv2.createTrackbar('HUE Max', 'Parameters', 190, 255, empty)
+	cv2.createTrackbar('SAT Min', 'Parameters', 195, 255, empty)
+	cv2.createTrackbar('SAT Max', 'Parameters', 255, 255, empty)
+	cv2.createTrackbar('VALUE Min', 'Parameters', 195, 255, empty)
+	cv2.createTrackbar('VALUE Max', 'Parameters', 255, 255, empty)
+	cv2.createTrackbar('Thresh1', 'Parameters', 0, 255, empty)
+	cv2.createTrackbar('Thresh2', 'Parameters', 225, 255, empty)
+
+
+read_temp = False
+db = pickle.load(save)
+try:
+	(h_min, h_max, s_min, s_max, v_min, v_max, threshold1, threshold2) = tuple(db.values())
+except:
+	read_temp = True 
+	takeVal()
+
 while True:
 	
 	_, img = cap.read()
+
+	if read_temp:
+		h_min = cv2.getTrackbarPos('HUE Min', 'Parameters')
+		h_max = cv2.getTrackbarPos('HUE Max', 'Parameters')
+		s_min = cv2.getTrackbarPos('SAT Min', 'Parameters')
+		s_max = cv2.getTrackbarPos('SAT Max', 'Parameters')
+		v_min = cv2.getTrackbarPos('VALUE Min', 'Parameters')
+		v_max = cv2.getTrackbarPos('VALUE Max', 'Parameters')
+		threshold1 = cv2.getTrackbarPos('Thresh1', 'Parameters') 
+		threshold2 = cv2.getTrackbarPos('Thresh2', 'Parameters')
+		db['h_min'], db['h_max'], db['s_min'], db['s_max'], db['v_min'], db['v_max'], db['threshold1'], db['threshold2'] = h_min, h_max, s_min, s_max, v_min, v_max, threshold1, threshold2
 
 	imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -60,10 +91,24 @@ while True:
 
 
 	if cv2.waitKey(1) &0xFF == ord('q'):
-		cv2.destroyAllWindows()
-		break
+		if read_temp:
+			cv2.destroyWindow('Parameters')
+			read_temp = False 
+		else:
+			break
 	elif cv2.waitKey(1) &0xFF == ord('p'):
-		(h_min, h_max, s_min, s_max, v_min, v_max, threshold1, threshold2) = parameters.takeVal()
+		db = {}
+		takeVal()
+		read_temp = True
+	elif (cv2.waitKey(1) &0xFF == ord('s')) and read_temp:
+		# writing the current values into save.dat
+		write = open('Values/save.dat', 'wb')
+		pickle.dump(db, write)
+		print('Saved')
+		write.close()
+		cv2.destroyWindow('Parameters')
 
+
+save.close()
 cap.release()
 cv2.destroyAllWindows()
